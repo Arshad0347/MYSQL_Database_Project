f62c59c980e40c1da35f93ef687f6850b3846cc5
Arshad0347 <arshad.eqbal009@gmail.com>
2025-05-28 12:29:07 +0530

    employee hiring

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const app=expres();
 app.use(expres.json());
 PORT=4300;
 
+//Employee hiring
+app.post('/addUser',(req,rest)=>{
+    const employee={
+        fullname:req.body.fullname,
+        salary:req.body.salary,
+        city:req.body.city,
+        email:req.body.email
+    }
+    // console.log(employee);
+    const sql="INSERT INTO `employeedata` SET ?";
+    connection.query(sql,employee,(error,result)=>{
+        if(error){
+            console.log(error);
+        }else(
+            console.log("Employee added successfully")
+        )
+    })
+    
+})
 
 
 app.listen(PORT,()=>console.log(`server is running on port ${PORT}`));
